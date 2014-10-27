2a217c1e112e9e6003fb584b0245cf2379e0ef2d
joelcoxokc <joelcox@hisimagination.com>
2014-10-27 12:26:07 -0700

    Update: Publish Version

--- gulpFile.js
+++ gulpFile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 
-var slushy = require('./index.js')(gulp);
+var gulp = require('./index.js')(gulp);
 
 
 gulp.task( 'store' , function(  ) {
   this.storage.create( 'hello', 'joel.json' );
   this.storage.set( 'me', 'joel' );
+  this.storage.set( 'this stuff', 'lakhdklaf;;asf' );
 
 })
 
